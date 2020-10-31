7bc06a458bd9736dac97f4778d0a402cd00257c6
Andrew Moses <andrewmosesdrive@gmail.com>
2020-10-31 15:12:35 -0700

    updated seed file to include references; began to update prompts and pseudo code out logic for functions

--- Assets/seed.sql
+++ Assets/seed.sql
@@ -5,33 +5,30 @@ create database cms_organization;
 use cms_organization;
 
 create table department (
-id int primary key,
+id int auto_increment not null primary key,
 dept_name varchar(30)
 );
 
 create table org_role (
-id int primary key,
+id int auto_increment not null primary key,
 title varchar(30),
 salary decimal,
-department_id int
+department_id int references department(id)
 );
 
 create table employee (
-id int primary key,
-first_name varchar(30),
-last_name varchar(30),
-org_role_id int,
-manager_id int
+id int auto_increment not null primary key,
+first_name varchar(30) not null,
+last_name varchar(30) not null,
+org_role_id int references org_role(id),
+manager_id int references employee(id)
 );
 
-insert into department values (1, "Engineering");
+-- test values
+insert into employee (first_name, last_name, org_role_id, manager_id) values ("Andrew", "Moses", 1, 1);
 
-insert into org_role values (1, "Software Engineer", 75000.00, 1);
-
-insert into employee values (1, "Andrew", "Moses", 1, 1);
-
-select * from employee 
-left join org_role 
-on employee.org_role_id = org_role.id 
-left join department
-on org_role.department_id = department.id;
+-- select * from employee 
+-- left join org_role 
+-- on employee.org_role_id = org_role.id 
+-- left join department
+-- on org_role.department_id = department.id;
--- main-server.js
+++ main-server.js
@@ -4,13 +4,10 @@ const inquirer = require("inquirer");
 const connection = mysql.createConnection({
   host: "localhost",
 
-  // Your port; if not 3306
   port: 3306,
 
-  // Your username
   user: "root",
 
-  // Your password
   password: "Gitgudfamlit21!",
   database: "cms_organization"
 });
@@ -22,6 +19,7 @@ connection.connect(function(err) {
 });
 
 const prompts = [
+    // first object to proc
     {
         type: "list",
         name: "initialPrompt",
@@ -29,16 +27,35 @@ const prompts = [
         choices: ["View", "Add", "Update", "Exit"]
     },
     {
+        // this object only procs if the user chooses "view"
         type: "list",
         name: "viewWhich",
         message: "What would you like to view?",
-        choices: ["Departments", "Roles", "Managers"],
+        choices: ["Departments", "Roles", "Managers", "Exit"],
         when: answers => {
             return answers.initialPrompt === "View"
         }
-        
-
     },
+    {
+        // this object only procs if the user chooses "add"
+        type: "list",
+        name: "addToChoice",
+        message: "Which business area would you like to add to?",
+        choices: ["Departments", "Roles", "Managers", "Exit"],
+        when: answers => {
+            return answers.initialPrompt === "Add"
+        }
+    },
+    {
+        // this object only procs if the user chooses "update"
+        type: "list",
+        name: "updateChoice",
+        message: "Which business area would you like to make updates to?",
+        choices: ["Departments", "Roles", "Managers", "Exit"],
+        when: answers => {
+            return answers.initialPrompt === "Update"
+        }
+    }
 ];
 
 async function runPrompt() {
@@ -46,6 +63,28 @@ async function runPrompt() {
         if(answers.initialPrompt === "Exit") {
             connection.end();
         }
+        else if(answers.viewWhich === "Departments") {
+            // show department information
+            console.table("Department information appears here")
+        }
+        else if(answers.viewWhich === "Roles") {
+            // show role information
+            console.table("Role information appears here")
+        }
+        else if(answers.viewWhich === "Managers") {
+            // show manager information
+            console.table("Manager information appears here")
+        }
+        else if(answers.addToChoice === "Departments") {
+            // need a function that will add to the "departments" table, maybe add new inquirer prompt
+        }
+        else if(answers.addToChoice === "Roles") {
+            // need a function that will add to the "roles" table, maybe add new inquirer prompt
+        }
+        else if(answers.addToChoice === "Managers") {
+            // need a function that will add to the "managers" table, maybe add new inquirer prompt here
+        }
+
         console.log("Successful test.")
     }).catch(error => {
         console.log(error)
